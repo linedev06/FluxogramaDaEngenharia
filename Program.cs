dc5501d6e0d5b9e689c7f1545ad6478a9570e84c
linedev06 <vitorafelismino@gmail.com>
2023-05-12 22:25:46 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿string texto;
+
+Console.WriteLine("--- Fluxograma de Engenharia ---");
+
+Console.Write("O objeto está se movendo (S/N)? ");
+bool R1 = Console.ReadLine()!.Substring(0,1).ToUpper() == "S";
+
+Console.Write("Deveria? ");
+bool R2 = Console.ReadLine()!.Substring(0,1).ToUpper() == "S";
+
+if (R1)
+{
+    if (R2)
+    {
+     Console.ForegroundColor = ConsoleColor.DarkGreen 
+     texto = ""
+    }
+}
